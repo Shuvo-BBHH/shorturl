a706b7e46cb6a17e62776295031661415b5fe61e
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2022-02-12 11:16:41 +0600

    Update mahdi.sh

--- mahdi.sh
+++ mahdi.sh
@@ -10,7 +10,7 @@ printf "\e[0m\e[1;93m   \___ \|  _ \ / _ \|  __| __| \e[0m\e[1;92m| |  | |  _  /
 printf "\e[0m\e[1;93m   ____) | | | | (_) | |  | |_  \e[0m\e[1;92m| |__| | | \ \| |____ \e[0m\n"
 printf "\e[0m\e[1;93m  |_____/|_| |_|\___/|_|   \__| \e[0m\e[1;92m \____/|_|  \_\______|\e[0m\n"
 printf "\e[0m\n"
-printf "\e[0m\e[1;93m        Created By Mahdi Hasan Shuvo \e[0m\e[1;91m( \e[0m\e[1;92mMahdi hasan \e[0m\e[1;91m)\e[0m\n"
+printf "\e[0m\e[1;93m        Created By Mahdi Hasan Shuvo \e[0m\e[1;91m(\e[0m\e[1;92mMahdi hasan \e[0m\e[1;91m)\e[0m\n"
 printf "\e[0m\n"
 printf "\e[0m\e[1;93m    Github :\e[0m\e[1;96m Shuvo-BBHH \e[0m\e[1;97m| \e[0m\e[1;93mInstagram :\e[0m\e[1;96m @mahdi\e[0m\n
 }
